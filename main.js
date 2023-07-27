e6017ebeea7ad2bb22392a42d4b716aa3d11880b
tw1ttt3r <tw1ttt3r@gmail.com>
2023-07-26 20:06:23 -0600

    Introduccion DOM

---
+++ main.js
@@ -0,0 +1,35 @@
+// getElementById
+// getElementsByClassName
+// getElementsByTagName
+
+// ejemplos
+// Obtener los nodos que tengan el id imagen
+const imagen1 = document.getElementById("imagen")
+// Obtener los nodos que tengan la clase datos
+const datos1 = document.getElementsByClassName("datos")
+// Obtener los nodos que tengan la clase photo
+const photo1 = document.getElementsByClassName("photo")
+// Obtener los nodos con etiqueta div
+const divs1 = document.getElementsByTagName("div")
+
+// querySelector
+// querySelectorAll
+
+// ejemplos
+// Obtener los nodos que tengan el id imagen
+const imagen2 = document.querySelector("#imagen")
+// elemento de tipo nodo
+// Obtener los nodos que tengan la clase datos
+const datos2 = document.querySelectorAll(".datos")
+// nodeList
+// Obtener los nodos que tengan la clase photo
+const photo2 = document.querySelectorAll(".photo")
+// Obtener los nodos con etiqueta div
+const divs2 = document.querySelectorAll("div")
+// nodeList
+
+// nodeList es un tipo de arreglo que especifica que contiene nodos
+
+
+
+
